cedc54858c1cddfad70540ab6de6e140dcf3d1d7
NIKITA <delya99@mail.ru>
2020-09-22 21:35:06 +0300

    Updates

---
+++ precache-manifest.fa2a538f4989e1a8d89feb640cdc4e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e9e55b876218aeb468370b1796a767",
+    "url": "/wedding-app/index.html"
+  },
+  {
+    "revision": "5ef740f7b3e19bfa8baa",
+    "url": "/wedding-app/static/css/main.5be08c62.chunk.css"
+  },
+  {
+    "revision": "ec2a301a61a3c72387cf",
+    "url": "/wedding-app/static/js/2.27641129.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/wedding-app/static/js/2.27641129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef740f7b3e19bfa8baa",
+    "url": "/wedding-app/static/js/main.550dd7e6.chunk.js"
+  },
+  {
+    "revision": "a4b34b1d73018a38f526",
+    "url": "/wedding-app/static/js/runtime-main.3e5c0d46.js"
+  }
+]);
